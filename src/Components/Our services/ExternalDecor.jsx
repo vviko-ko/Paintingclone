637d8ea8c0cc9b87bc023b731e243354a170bcd6
Victor Olusanmi-Sogbein <olusanmiv@gmail.com>
2025-03-29 17:47:07 +0100

    feat: upgrade of code

--- src/Components/Our services/ExternalDecor.jsx
+++ src/Components/Our services/ExternalDecor.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function ExternalDecor() {
   return (
     <main>
-      <h1>External Decorating Services</h1>
-      <p>We offer high-quality exterior decorating services...</p>
+     
     </main>
   );
 }
